0e41d3d283f731bb46027a266e503999d5338c07
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-09-16 18:18:17 +0900

    fix: main 수정 - hot reload 되도록

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(backgroundColor: Colors.teal, body: Container()),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.teal, body: Container());
+  }
 }
