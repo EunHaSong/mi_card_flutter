86667bd22464bc1a21a50cfa9c13ceaf0f544b0f
EunHaSong <shaeun03290@sookmyung.ac.kr>
2025-10-08 22:53:22 +0900

    Feat. 프로필 ui 추가 완료

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(backgroundColor: Colors.teal, body: Container()),
+      home: Scaffold(
+        backgroundColor: Colors.indigo[300],
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              // backgroundColor: Colors.white70,)
+              backgroundImage: AssetImage('images/songhaeun.png'),
+            ),
+            Text(
+              "SongHaeun",
+              style: TextStyle(
+                fontFamily: 'Sacramento',
+                fontWeight: FontWeight.bold,
+                fontSize: 45.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              "ARTIFICIAL INTELLIGENCE ENGINEERING",
+              style: TextStyle(
+                fontFamily: 'IBM',
+                fontSize: 15.0,
+                letterSpacing: 2.5,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(color: Colors.indigo[100]),
+            ),
+            SizedBox(height: 15.0),
+            Card(
+              color: Colors.white,
+              // padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.indigo),
+                  title: Text(
+                    '+82 010-6300-5850',
+                    style: TextStyle(
+                      fontFamily: 'IBM',
+                      fontSize: 16.0,
+                      letterSpacing: 2.0,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              // padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+              child: Padding(
+                padding: const EdgeInsets.all(3.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.indigo),
+                  title: Text(
+                    'shaeun03290@sookmyung.ac.kr',
+                    style: TextStyle(
+                      fontFamily: 'IBM',
+                      fontSize: 16.0,
+                      letterSpacing: 2.0,
+                      color: Colors.indigo,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30.0),
+          ],
+        ),
+      ),
     );
   }
 }
